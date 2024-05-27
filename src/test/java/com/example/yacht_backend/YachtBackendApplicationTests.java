97ca5767bfd943f599131946f407a326aa4b6095
강인구 <ingu9981@gmail.com>
2024-05-27 22:13:25 +0900

    최초 세팅 커밋

---
+++ src/test/java/com/example/yacht_backend/YachtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yacht_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
