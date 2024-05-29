ab343122e0677114b72c12255e005c9417e2fc9c
강인구 <ingu9981@gmail.com>
2024-05-29 23:52:00 +0900

    feat: RoomRepository에 hostuser를 통해 찾는 핸들러 추가

--- src/main/java/com/example/yacht_backend/repository/RoomRepository.java
+++ src/main/java/com/example/yacht_backend/repository/RoomRepository.java
@@ -3,7 +3,9 @@ package com.example.yacht_backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.yacht_backend.model.Room;
+import java.util.List;
+
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    
+    List<Room> findByHostUser(String hostUser);
 }
