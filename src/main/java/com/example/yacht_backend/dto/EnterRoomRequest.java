ca8080626c5b70d26e8db4850512906676505243
강인구 <ingu9981@gmail.com>
2024-05-30 03:49:22 +0900

    fix: EnterRoomRequest 수정

--- src/main/java/com/example/yacht_backend/dto/EnterRoomRequest.java
+++ src/main/java/com/example/yacht_backend/dto/EnterRoomRequest.java
@@ -3,11 +3,13 @@ package com.example.yacht_backend.dto;
 
 public class EnterRoomRequest {
     private String roomId;
+    private String userId;
 
     public EnterRoomRequest() {}
 
-    public EnterRoomRequest(String roomId) {
+    public EnterRoomRequest(String roomId, String userId) {
         this.roomId = roomId;
+        this.userId = userId;
     }
     
     public String getRoomId() {
@@ -16,5 +18,13 @@ public class EnterRoomRequest {
 
     public void setRoomId(String roomId) {
         this.roomId = roomId;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
