bfba52e5708a8a2c60b1a0b42523b4d79d48ee31
강인구 <ingu9981@gmail.com>
2024-05-30 04:45:50 +0900

    fix: WebSocketHandler생성자 변경에 따른 수정

--- src/main/java/com/example/yacht_backend/websocket/WebSocketConfig.java
+++ src/main/java/com/example/yacht_backend/websocket/WebSocketConfig.java
@@ -6,19 +6,22 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import com.example.yacht_backend.repository.RoomRepository;
+import com.example.yacht_backend.service.ApiService;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     private final RoomRepository roomRepository;
+    private final ApiService apiService;
 
-    WebSocketConfig(RoomRepository roomRepository) {
+    WebSocketConfig(RoomRepository roomRepository, ApiService apiService) {
         this.roomRepository = roomRepository;
+        this.apiService = apiService;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // TODO: setAllowedOrigins("*")를 수정
-        registry.addHandler(new WebSocketHandler(roomRepository), "/ws").setAllowedOrigins("*");
+        registry.addHandler(new WebSocketHandler(roomRepository, apiService), "/ws").setAllowedOrigins("*");
     }
 }
