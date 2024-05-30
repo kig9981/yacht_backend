79f69533b8d432266045a79bbab8846586623961
강인구 <ingu9981@gmail.com>
2024-05-31 00:56:45 +0900

    fix: createNewRoom 작성

--- src/main/java/com/example/yacht_backend/service/ApiService.java
+++ src/main/java/com/example/yacht_backend/service/ApiService.java
@@ -2,20 +2,19 @@ package com.example.yacht_backend.service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.yacht_backend.exception.RoomNotFoundException;
+import org.springframework.web.context.request.async.DeferredResult;
 import com.example.yacht_backend.model.Room;
 import com.example.yacht_backend.websocket.WebSocketHandler;
-
-import jakarta.persistence.EntityNotFoundException;
+import com.example.yacht_backend.dto.CreateNewRoomResponse;
 
 
 @Service
 public class ApiService {
     private final ApiDatabaseService apiDatabaseService;
+    private final ConcurrentHashMap<String, DeferredResult<String>> roomGuestMap = new ConcurrentHashMap<>();
 
     ApiService(ApiDatabaseService apiDatabaseService) {
         this.apiDatabaseService = apiDatabaseService;
@@ -25,19 +24,34 @@ public class ApiService {
         return apiDatabaseService.findAll();
     }
     
-    public String createNewRoom(String userId) {
+    public CreateNewRoomResponse createNewRoom(String userId) {
         Room hostRoom = apiDatabaseService.findRoomByHostUserId(userId);
-        if (hostRoom != null) {
-            return hostRoom.getRoomId();
-        }
         Room guestRoom = apiDatabaseService.findRoomByGuestUserId(userId);
         if (guestRoom != null) {
-            return null;
+            DeferredResult<String> guestId = new DeferredResult<String>(60000L);
+            guestId.setResult(null);
+            return new CreateNewRoomResponse(null, guestId);
         }
-        String roomId = UUID.randomUUID().toString();
-        Room newRoom = new Room(roomId, userId, null);
-        apiDatabaseService.save(newRoom);
-        return roomId.toString();
+        String tempRoomId = null;
+        if (hostRoom != null) {
+            tempRoomId = hostRoom.getRoomId();
+        }
+        else {
+            tempRoomId = UUID.randomUUID().toString();
+            Room newRoom = new Room(tempRoomId, userId, null);
+            apiDatabaseService.save(newRoom);
+        }
+        String roomId = tempRoomId;
+        DeferredResult<String> guestId = new DeferredResult<String>(60000L);
+        
+        roomGuestMap.put(roomId, guestId);
+        guestId.onTimeout(() -> {
+            synchronized (guestId) {
+                roomGuestMap.remove(roomId);
+                guestId.setResult(null);
+            }
+        });
+        return new CreateNewRoomResponse(roomId, guestId);
     }
 
     public String enterRoom(String roomId, String userId) throws Exception {
