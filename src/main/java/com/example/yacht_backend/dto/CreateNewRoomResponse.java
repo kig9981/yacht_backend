f206b753482dadead3de833f21cdf75492e691f9
강인구 <ingu9981@gmail.com>
2024-05-31 21:25:40 +0900

    feat: CreateNewRoomResponse에 data필드 추가

--- src/main/java/com/example/yacht_backend/dto/CreateNewRoomResponse.java
+++ src/main/java/com/example/yacht_backend/dto/CreateNewRoomResponse.java
@@ -5,12 +5,14 @@ import org.springframework.web.context.request.async.DeferredResult;
 public class CreateNewRoomResponse {
     private String roomId;
     private DeferredResult<String> guestUserId;
+    private DeferredResult<String> data;
 
     public CreateNewRoomResponse() {}
 
-    public CreateNewRoomResponse(String roomId, DeferredResult<String> guestUserId) {
+    public CreateNewRoomResponse(String roomId, DeferredResult<String> guestUserId, DeferredResult<String> data) {
         this.roomId = roomId;
         this.guestUserId = guestUserId;
+        this.data = data;
     }
 
     public String getRoomId() {
@@ -27,5 +29,13 @@ public class CreateNewRoomResponse {
 
     public void setGuestUserId(DeferredResult<String> guestUserId) {
         this.guestUserId = guestUserId;
+    }
+
+    public DeferredResult<String> getData() {
+        return this.data;
+    }
+
+    public void setData(DeferredResult<String> data) {
+        this.data = data;
     }
 }
