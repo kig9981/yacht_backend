42f45da047fb016c054d7133bb5afc66c773ccd9
강인구 <ingu9981@gmail.com>
2024-05-31 21:13:16 +0900

    feat: UserRepository에 쿼리 추가

--- src/main/java/com/example/yacht_backend/repository/UserRepository.java
+++ src/main/java/com/example/yacht_backend/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.yacht_backend.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.yacht_backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    List<User> findByUserId(String userId);
+    List<User> findBySessionId(String sessionId);
 }
