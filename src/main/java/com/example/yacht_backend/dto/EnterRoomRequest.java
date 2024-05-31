06812a3e9a878122c6fc4580be57d62b7d3614c3
강인구 <ingu9981@gmail.com>
2024-05-31 21:41:42 +0900

    feat: EnterRoomRequest에 data필드 추가

--- src/main/java/com/example/yacht_backend/dto/EnterRoomRequest.java
+++ src/main/java/com/example/yacht_backend/dto/EnterRoomRequest.java
@@ -3,11 +3,13 @@ package com.example.yacht_backend.dto;
 
 public class EnterRoomRequest {
     private String sessionId;
+    private String data;
 
     public EnterRoomRequest() {}
 
-    public EnterRoomRequest(String sessionId) {
+    public EnterRoomRequest(String sessionId, String data) {
         this.sessionId = sessionId;
+        this.data = data;
     }
 
     public String getSessionId() {
@@ -16,5 +18,13 @@ public class EnterRoomRequest {
 
     public void setSessionId(String sessionId) {
         this.sessionId = sessionId;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 }
