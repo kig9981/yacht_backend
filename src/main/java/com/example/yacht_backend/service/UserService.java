a095c591f9c6ca2d31290df4125f44a66e88a12a
강인구 <ingu9981@gmail.com>
2024-05-31 21:03:16 +0900

    feat: UserService.createUser 작성

--- src/main/java/com/example/yacht_backend/service/UserService.java
+++ src/main/java/com/example/yacht_backend/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.yacht_backend.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
-import com.example.yacht_backend.repository.UserRepository;
+import com.example.yacht_backend.model.User;
 
 @Service
 public class UserService {
@@ -10,5 +12,12 @@ public class UserService {
 
     UserService(UserDatabaseService userDatabaseService) {
         this.userDatabaseService = userDatabaseService;
+    }
+
+    public User createUser() {
+        String userId = UUID.randomUUID().toString();
+        String sessionId = UUID.randomUUID().toString();
+        User user = new User(userId, sessionId);
+        return userDatabaseService.save(user);
     }
 }
