5a8587ed94382c3895351f46223c3d0934550816
강인구 <ingu9981@gmail.com>
2024-05-31 22:37:49 +0900

    fix: UserController 분리

--- src/main/java/com/example/yacht_backend/controller/ApiController.java
+++ src/main/java/com/example/yacht_backend/controller/ApiController.java
@@ -1,20 +1,7 @@
 package com.example.yacht_backend.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.yacht_backend.dto.CreateNewRoomRequest;
-import com.example.yacht_backend.dto.CreateNewRoomResponse;
-import com.example.yacht_backend.dto.EnterRoomRequest;
-import com.example.yacht_backend.dto.EnterRoomResponse;
-import com.example.yacht_backend.model.Room;
-import com.example.yacht_backend.model.User;
-import com.example.yacht_backend.service.RoomService;
-import com.example.yacht_backend.service.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,18 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class ApiController {
-    private final RoomService roomService;
-    private final UserService userService;
-
-    ApiController(RoomService roomService, UserService userService) {
-        this.roomService = roomService;
-        this.userService = userService;
-    }
-
-    @PostMapping("/users")
-    public User createUser() {
-        return userService.createUser();
-    }
 
     @DeleteMapping("/leave-room")
     public void leaveRoom(@RequestBody String entity) {
