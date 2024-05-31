e9fce69f7635e6a3f70f8a302a0cf0c5ff84bb59
강인구 <ingu9981@gmail.com>
2024-05-31 22:06:44 +0900

    feat: CreateNewRoomRequest에도 data 추가

--- src/main/java/com/example/yacht_backend/dto/CreateNewRoomRequest.java
+++ src/main/java/com/example/yacht_backend/dto/CreateNewRoomRequest.java
@@ -2,11 +2,13 @@ package com.example.yacht_backend.dto;
 
 public class CreateNewRoomRequest {
     private String sessionId;
+    private String data;
 
     public CreateNewRoomRequest() {}
 
-    public CreateNewRoomRequest(String sessionId) {
+    public CreateNewRoomRequest(String sessionId, String data) {
         this.sessionId = sessionId;
+        this.data = data;
     }
 
     public String getSessionId() {
@@ -15,5 +17,13 @@ public class CreateNewRoomRequest {
 
     public void setSessionId(String sessionId) {
         this.sessionId = sessionId;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 }
